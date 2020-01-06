02f127df66c57cf7f8f3ff602287bdcd99adae05
Placide IRANDORA <placideirandora@gmail.com>
2020-01-06 20:40:34 +0200

    Setup a SwiftUI project

---
+++ calculator/calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Placide IRANDORA on 06/01/2020.
+//  Copyright © 2020 Placide IRANDORA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

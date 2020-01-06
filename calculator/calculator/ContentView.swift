dff77c215410b01a67d12d711e06a6b147970d55
Placide IRANDORA <placideirandora@gmail.com>
2020-01-06 22:07:27 +0200

    Display the calculator content from a two-dimensional array

--- calculator/calculator/ContentView.swift
+++ calculator/calculator/ContentView.swift
@@ -9,19 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Two dimensional array
+    let buttons: Array = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
         VStack {
-            HStack {
-                ForEach(["7", "8", "9", "X"], id: \.self) { button in
-                    Text(button)
-                        .font(.system(size: 35))
-                    .frame(width: 80, height: 80)
+            // Display the buttons form the dimensional array
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { button in
+                        Text(button)
+                            .font(.system(size: 35))
+                                .frame(width: 80, height: 80)
+                    }
                 }
             }
         }
     }
 }
 
+// Display the content 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()

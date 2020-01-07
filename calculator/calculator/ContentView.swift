3b6a10eb396bd14943aa8877cc868e72e505074d
Placide IRANDORA <placideirandora@gmail.com>
2020-01-07 16:50:00 +0200

    Refactor to use an efficient method: enum, and display the background colors accordingly

--- calculator/calculator/ContentView.swift
+++ calculator/calculator/ContentView.swift
@@ -8,14 +8,95 @@
 
 import SwiftUI
 
+enum CalculatorButton: String {
+    case zero, one, two, three, four
+    case five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero:
+            return "0"
+        
+        case .one:
+            return "1"
+        
+        case .two:
+            return "2"
+        
+        case .three:
+            return "3"
+            
+        case .four:
+            return "4"
+            
+        case .five:
+            return "5"
+        
+        case .six:
+            return "6"
+        
+        case .seven:
+            return "7"
+        
+        case .eight:
+            return "8"
+        
+        case .nine:
+            return "9"
+        
+        case .ac:
+            return "AC"
+        
+        case .plusMinus:
+            return "+/-"
+        
+        case .percent:
+            return "%"
+            
+        case .equals:
+            return "="
+            
+        case .plus:
+            return "+"
+            
+        case .minus:
+            return "-"
+        
+        case .multiply:
+            return "x"
+        
+        case .divide:
+            return "/"
+            
+        default:
+            return ""
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return Color(.darkGray)
+            
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+            
+        default:
+            return Color(.orange)
+        }
+    }
+}
+
 struct ContentView: View {
     
     // Two dimensional array
-    let buttons: Array = [
-        ["7", "8", "9", "x"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus]
     ]
     
     var body: some View {
@@ -34,11 +115,11 @@ struct ContentView: View {
                 ForEach(buttons, id: \.self) { row in
                     HStack {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
+                            Text(button.title)
                                 .font(.system(size: 35))
                                 .frame(width: 80, height: 80)
                                 .foregroundColor(.white)
-                                .background(Color.orange)
+                                .background(button.backgroundColor)
                                 .cornerRadius(50)
                                     
                         }
